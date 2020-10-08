075d3e00322771b456fee38c478826bf6e69a408
dfzfz7 <62215554+dfzfz7@users.noreply.github.com>
2020-10-08 19:30:24 +0200

    DTO + DAO + Service + Controller + MongoDB + data sample

---
+++ e4SpringDiceGameMongodb/src/main/java/com/dicegame/mongodb/E4SpringDiceGameMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.dicegame.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E4SpringDiceGameMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E4SpringDiceGameMongodbApplication.class, args);
+	}
+
+}
